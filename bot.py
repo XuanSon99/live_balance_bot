46dfdd35e97a9c974290b596403a8ad70f25dd0b
Your Name <you@example.com>
2024-01-05 11:32:41 +0700

    chnage

--- bot.py
+++ bot.py
@@ -77,6 +77,6 @@ app.add_handler(MessageHandler(filters.ALL, messageHandler))
 
 job_queue = app.job_queue
 
-job_minute = job_queue.run_repeating(callback_minute, interval=60, first=10)
+job_minute = job_queue.run_repeating(callback_minute, interval=90, first=10)
 
 app.run_polling()
